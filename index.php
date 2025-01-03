26ec961499babba7c0a874193b0c2c25969351af
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2025-01-02 22:09:05 -0300

    Alterando title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfólio</title>
+    <title>Douglas Almeida ;)</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="css/style.css">
 
